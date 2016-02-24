354d802af670462c6ad2ed12f00abc71c9c7ff40
Jordan Duabe <jordan.duabe@cloudsherpas.com>
2016-02-24 22:10:30 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+ci-automation
+=============
+
+CI (Continuous Integration) setup automation script using (Fabric)[http://www.fabfile.org/].
+
+### Usage:
+
+`fab -H <host1,host2> <function>`
---
+++ fabfile.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+def configure_env():
+    create_tools_dir()
+    install_jdk_7()
+    install_nginx()
+    install_nexus()
+    install_sonarqube()
+    install_jenkins()
+
+
+def create_tools_dir():
+    pass
+
+
+def install_jdk_7():
+    pass
+
+
+def install_nginx():
+    pass
+
+
+def install_jenkins():
+    pass
+
+
+def install_nexus():
+    pass
+
+
+def install_sonarqube():
+    pass
+
