38f485a7a7b1b4bc55bd6c831c60c19f7fbada13
Ben Sarmiento <ben.sarmiento@cloudsherpas.com>
2016-03-11 01:49:46 +0800

    Add docker install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# install Docker
+curl -fsSL https://get.docker.com/ | sh
+
+# install Docker Compose
+# NOTE: execute as root!
+curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
