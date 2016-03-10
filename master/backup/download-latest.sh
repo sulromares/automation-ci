3c949838af6873922e01c1bab9fb5d1a158f1b87
Ben Sarmiento <ben.sarmiento@cloudsherpas.com>
2016-03-11 04:57:32 +0800

    Add script for downloading latest backups

---
+++ master/backup/download-latest.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+token=$(python ../../utils/access_token.py)
+
+echo "Fetching jenkins data..."
+
+curl -s -H "Authorization: Bearer ${token}" https://storage.googleapis.com/javelin-ci-backup-test/jenkins_data.tar.gz
+echo "OK"
+
+echo "Fetching nexus data..."
+
+curl -s -H "Authorization: Bearer ${token}" https://storage.googleapis.com/javelin-ci-backup-test/nexus_data.tar.gz
+echo "OK"
+
+echo "Fetching sonar data..."
+
+curl -s -H "Authorization: Bearer ${token}" https://storage.googleapis.com/javelin-ci-backup-test/sonar_data.tar.gz
+echo "OK"
+
+echo "Fetching postgres data..."
+
+curl -s -H "Authorization: Bearer ${token}" https://storage.googleapis.com/javelin-ci-backup-test/postgres_data.tar.gz
+echo "OK"
+
+echo "Done!"
