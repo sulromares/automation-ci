a2d997ca9fc2b3ebfa1ab135826b2524e0f0041b
Ben Sarmiento <ben.sarmiento@cloudsherpas.com>
2016-03-11 05:33:46 +0800

    Bucket name on a var

--- master/backup/backup-data.sh
+++ master/backup/backup-data.sh
@@ -3,13 +3,14 @@
 now=$(date +%Y%m%d%H%M%S)
 token=$(python ../../utils/access_token.py)
 prefix=$(basename -- "$(dirname -- "$(pwd)")")
+bucket="javelin-ci-backup-test"
 
 echo "Backing up jenkins data..."
 
 docker run --rm --volumes-from ${prefix}_jenkins_1 -v $(pwd):/backup ubuntu tar czf /backup/jenkins_data_${now}.tar.gz /var/jenkins_home
 
 echo "Uploading to GCS..."
-curl -s -H "Authorization: Bearer ${token}" -H "Content-Type: application/x-gzip" --data-binary @jenkins_data_${now}.tar.gz "https://www.googleapis.com/upload/storage/v1/b/javelin-ci-backup-test/o?uploadType=media&name=jenkins_data_${now}.tar.gz"
+curl -s -H "Authorization: Bearer ${token}" -H "Content-Type: application/x-gzip" --data-binary @jenkins_data_${now}.tar.gz "https://www.googleapis.com/upload/storage/v1/b/${bucket}/o?uploadType=media&name=jenkins_data_${now}.tar.gz"
 rm -f jenkins_data_${now}.tar.gz
 echo "OK"
 
@@ -18,7 +19,7 @@ echo "Backing up nexus data..."
 docker run --rm --volumes-from ${prefix}_nexus_1 -v $(pwd):/backup ubuntu tar czf /backup/nexus_data_${now}.tar.gz /sonatype-work
 
 echo "Uploading to GCS..."
-curl -s -H "Authorization: Bearer ${token}" -H "Content-Type: application/x-gzip" --data-binary @nexus_data_${now}.tar.gz "https://www.googleapis.com/upload/storage/v1/b/javelin-ci-backup-test/o?uploadType=media&name=nexus_data_${now}.tar.gz"
+curl -s -H "Authorization: Bearer ${token}" -H "Content-Type: application/x-gzip" --data-binary @nexus_data_${now}.tar.gz "https://www.googleapis.com/upload/storage/v1/b/${bucket}/o?uploadType=media&name=nexus_data_${now}.tar.gz"
 rm -f nexus_data_${now}.tar.gz
 echo "OK"
 
@@ -27,7 +28,7 @@ echo "Backing up sonar data..."
 docker run --rm --volumes-from ${prefix}_sonar_1 -v $(pwd):/backup ubuntu tar czf /backup/sonar_data_${now}.tar.gz /opt/sonarqube/extensions /opt/sonarqube/lib/bundled-plugins
 
 echo "Uploading to GCS..."
-curl -s -H "Authorization: Bearer ${token}" -H "Content-Type: application/x-gzip" --data-binary @sonar_data_${now}.tar.gz "https://www.googleapis.com/upload/storage/v1/b/javelin-ci-backup-test/o?uploadType=media&name=sonar_data_${now}.tar.gz"
+curl -s -H "Authorization: Bearer ${token}" -H "Content-Type: application/x-gzip" --data-binary @sonar_data_${now}.tar.gz "https://www.googleapis.com/upload/storage/v1/b/${bucket}/o?uploadType=media&name=sonar_data_${now}.tar.gz"
 rm -f sonar_data_${now}.tar.gz
 echo "OK"
 
@@ -36,7 +37,7 @@ echo "Backing up postgres data..."
 docker run --rm --volumes-from ${prefix}_postgres_1 -v $(pwd):/backup ubuntu tar czf /backup/postgres_data_${now}.tar.gz /var/lib/postgresql
 
 echo "Uploading to GCS..."
-curl -s -H "Authorization: Bearer ${token}" -H "Content-Type: application/x-gzip" --data-binary @postgres_data_${now}.tar.gz "https://www.googleapis.com/upload/storage/v1/b/javelin-ci-backup-test/o?uploadType=media&name=postgres_data_${now}.tar.gz"
+curl -s -H "Authorization: Bearer ${token}" -H "Content-Type: application/x-gzip" --data-binary @postgres_data_${now}.tar.gz "https://www.googleapis.com/upload/storage/v1/b/${bucket}/o?uploadType=media&name=postgres_data_${now}.tar.gz"
 rm -f postgres_data_${now}.tar.gz
 echo "OK"
 
--- master/backup/download-latest.sh
+++ master/backup/download-latest.sh
@@ -1,25 +1,26 @@
 #!/bin/sh
 
 token=$(python ../../utils/access_token.py)
+bucket="javelin-ci-backup-test"
 
 echo "Fetching jenkins data..."
 
-curl -s -H "Authorization: Bearer ${token}" "https://storage.googleapis.com/javelin-ci-backup-test/jenkins_data.tar.gz"
+curl -s -H "Authorization: Bearer ${token}" "https://storage.googleapis.com/${bucket}/jenkins_data.tar.gz"
 echo "OK"
 
 echo "Fetching nexus data..."
 
-curl -s -H "Authorization: Bearer ${token}" "https://storage.googleapis.com/javelin-ci-backup-test/nexus_data.tar.gz"
+curl -s -H "Authorization: Bearer ${token}" "https://storage.googleapis.com/${bucket}/nexus_data.tar.gz"
 echo "OK"
 
 echo "Fetching sonar data..."
 
-curl -s -H "Authorization: Bearer ${token}" "https://storage.googleapis.com/javelin-ci-backup-test/sonar_data.tar.gz"
+curl -s -H "Authorization: Bearer ${token}" "https://storage.googleapis.com/${bucket}/sonar_data.tar.gz"
 echo "OK"
 
 echo "Fetching postgres data..."
 
-curl -s -H "Authorization: Bearer ${token}" "https://storage.googleapis.com/javelin-ci-backup-test/postgres_data.tar.gz"
+curl -s -H "Authorization: Bearer ${token}" "https://storage.googleapis.com/${bucket}/postgres_data.tar.gz"
 echo "OK"
 
 echo "Done!"
