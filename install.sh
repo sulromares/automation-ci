23d3e7450f11f4db3f49e3b4fdc94bae3c1dd86e
Ben Sarmiento <ben.sarmiento@cloudsherpas.com>
2016-03-11 01:50:50 +0800

    Execute as root

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ curl -fsSL https://get.docker.com/ | sh
 
 # install Docker Compose
 # NOTE: execute as root!
+sudo -i
 curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
