5088891b6daf7317fca245488d7db1ccb2357cd4
Ben Sarmiento <ben.sarmiento@cloudsherpas.com>
2016-03-11 05:32:01 +0800

    Wrap urls in quotes 2

--- master/backup/download-latest.sh
+++ master/backup/download-latest.sh
@@ -4,22 +4,22 @@ token=$(python ../../utils/access_token.py)
 
 echo "Fetching jenkins data..."
 
-curl -s -H "Authorization: Bearer ${token}" https://storage.googleapis.com/javelin-ci-backup-test/jenkins_data.tar.gz
+curl -s -H "Authorization: Bearer ${token}" "https://storage.googleapis.com/javelin-ci-backup-test/jenkins_data.tar.gz"
 echo "OK"
 
 echo "Fetching nexus data..."
 
-curl -s -H "Authorization: Bearer ${token}" https://storage.googleapis.com/javelin-ci-backup-test/nexus_data.tar.gz
+curl -s -H "Authorization: Bearer ${token}" "https://storage.googleapis.com/javelin-ci-backup-test/nexus_data.tar.gz"
 echo "OK"
 
 echo "Fetching sonar data..."
 
-curl -s -H "Authorization: Bearer ${token}" https://storage.googleapis.com/javelin-ci-backup-test/sonar_data.tar.gz
+curl -s -H "Authorization: Bearer ${token}" "https://storage.googleapis.com/javelin-ci-backup-test/sonar_data.tar.gz"
 echo "OK"
 
 echo "Fetching postgres data..."
 
-curl -s -H "Authorization: Bearer ${token}" https://storage.googleapis.com/javelin-ci-backup-test/postgres_data.tar.gz
+curl -s -H "Authorization: Bearer ${token}" "https://storage.googleapis.com/javelin-ci-backup-test/postgres_data.tar.gz"
 echo "OK"
 
 echo "Done!"
