bb6de893abadc159ebf705913b03110b4af8d36e
Ben Sarmiento <ben.sarmiento@cloudsherpas.com>
2016-03-14 22:59:54 +0800

    Install oauth2client

--- install.sh
+++ install.sh
@@ -8,3 +8,7 @@ curl -fsSL https://get.docker.com/ | sh
 sudo -i
 curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+# install utils
+apt-get install python-pip
+pip install --upgrade google-api-python-client
