547f340f9371212bd59074937841d7827c6f52f8
Alvin Llobrera <alvin.llobrera@cloudsherpas.com>
2016-03-16 00:12:38 +0800

    Change args to constants instead of a function

--- fabfile.py
+++ fabfile.py
@@ -5,6 +5,23 @@ from fabric.api import cd, run, sudo
 from fabric.contrib.files import exists
 from fabric.operations import prompt
 
+ARGS = ('NEXUS_RELEASE_SERVER_ID',
+        'NEXUS_RELEASE_SERVER_UNAME',
+        'NEXUS_RELEASE_SERVER_PASSWD',
+        'NEXUS_SNAPSHOT_SERVER_ID',
+        'NEXUS_SNAPSHOT_SERVER_UNAME',
+        'NEXUS_SNAPSHOT_SERVER_PASSWD',
+        'NEXUS_URL',
+        'SONAR_LOGIN',
+        'SONAR_PASSWD',
+        'SONAR_JDBC_URL',
+        'SONAR_JDBC_UNAME',
+        'SONAR_JDBC_PASSWD',
+        'SONAR_HOST_URL',
+        'GCLOUD_SERVICE_ACCOUNT_EMAIL',
+        'GCLOUD_SERVICE_ACCOUNT_AUTH_URL'
+        )
+
 
 # master instance setup
 def master():
@@ -22,11 +39,12 @@ def slave():
     clone_ci_automation()
     install_deps("slave")
     with cd('$HOME/docker-script/ci-automation/slave'):
-        run('echo Note: The docker-compose.yml args must have correct values.', quiet=True)
+        run('echo Note: The docker-compose.yml args must have correct values.',
+            quiet=True)
         response = prompt('Is the docker-compose.yml already updated (y/N) ?')
 
         if response not in ['Y', 'y']:
-            for arg in get_list_of_args():
+            for arg in ARGS:
                 response = prompt("Enter value for %s:" % arg)
                 populate_slave_arg(arg + "_VAL", response)
 
@@ -42,21 +60,13 @@ def clone_ci_automation():
 
 def install_deps(ci_env):
     with cd('$HOME/docker-script/ci-automation'):
-        if env == "master":
+        if ci_env == "master":
             run('sh install.sh master')
-        elif env == "slave":
+        elif ci_env == "slave":
             run('sh install.sh')
 
 
 def populate_slave_arg(arg_key, arg_val):
-    run('sed -i "s|%s|%s|g" docker-compose.yml' % (arg_key, arg_val), quiet=True)
+    run('sed -i "s|%s|%s|g" docker-compose.yml' % (arg_key, arg_val),
+        quiet=True)
 
-
-def get_list_of_args():
-    return ['NEXUS_RELEASE_SERVER_ID', 'NEXUS_RELEASE_SERVER_UNAME',
-            'NEXUS_RELEASE_SERVER_PASSWD', 'NEXUS_SNAPSHOT_SERVER_ID',
-            'NEXUS_SNAPSHOT_SERVER_UNAME', 'NEXUS_SNAPSHOT_SERVER_PASSWD',
-            'NEXUS_URL', 'SONAR_LOGIN', 'SONAR_PASSWD', 'SONAR_JDBC_URL',
-            'SONAR_JDBC_UNAME', 'SONAR_JDBC_PASSWD', 'SONAR_HOST_URL',
-            'GCLOUD_SERVICE_ACCOUNT_EMAIL', 'GCLOUD_SERVICE_ACCOUNT_AUTH_URL'
-            ]
