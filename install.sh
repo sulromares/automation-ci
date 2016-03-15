dd1d6d04392b3969eb927513ff2942476c1f77f0
Jordan Duabe <jordan.duabe@cloudsherpas.com>
2016-03-16 00:04:18 +0800

    Add validation to install.sh script
    - Tested if docker and docker-compose are already existing.
    
    - Added checking for the environment flag which only installs the
      google-api-python-client module on 'master'.

--- install.sh
+++ install.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
 
 # install Docker
-curl -fsSL https://get.docker.com/ | sh
+[ ! -f /usr/bin/docker ] && curl -fsSL https://get.docker.com/ | sh
 
 # install Docker Compose
 # NOTE: execute as root!
 sudo su -
-curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+if [ ! -f /usr/local/bin/docker-compose ]; then
+  curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
+fi
 
 # install utils
-apt-get install python-pip
-pip install --upgrade google-api-python-client
+if [ "$1" = "master" ]; then
+    pip -V 2>&1 > /dev/null
+    is_pip_installed=$?
+
+    if [ $is_pip_installed -eq 0 ]; then
+      pip show google-api-python-client 2>&1 > /dev/null
+      is_gapi_py_client_installed=$?
+
+      if [ $is_gapi_py_client_installed -ne 0 ]; then
+        pip install --upgrade google-api-python-client
+      fi
+    else
+      apt-get install python-pip
+    fi
+fi
